ab030ff710a2c1bd7d506f045d81d1ad820933ba
JT <jt@gosquared.com>
2014-05-06 10:26:44 +0100

    Update nginx version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-set[:nginx][:version]               = "1.4.*"
-set[:nginx][:source]                = "http://nginx.org/download/nginx-1.4.1.tar.gz"
+set[:nginx][:version]               = "1.6.*"
+set[:nginx][:source]                = "http://nginx.org/download/nginx-1.6.0.tar.gz"
 set[:nginx][:apt_packages]          = %w[nginx-common nginx-full nginx]
 
 default[:nginx][:dir]               = "/etc/nginx"
